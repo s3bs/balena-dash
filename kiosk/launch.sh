b4f7509418493ec489d52389655717025262fb7b
Sebastian <s3bs@users.noreply.github.com>
2021-01-03 14:39:14 +0100

    Update launch.sh

--- kiosk/launch.sh
+++ kiosk/launch.sh
@@ -6,14 +6,25 @@ sleep 3
 # delete the last line in xstart script and replace with new settings
 sed -i '$d' /home/chromium/xstart.sh
 
+if [ ! -z ${KIOSK_DEBUG+x} && [ "$KIOSK_DEBUG+x" -eq 1 ]
+ then
+   set -x
+fi
+
+_LOCAL_URL="$1"
+
+if [ ! -z ${JITSI+x} ] && [ "$JITSI" -eq "1" ]
+  then
+    _LOCAL_URL="$_LOCAL_URL"'#config.prejoinPageEnabled=false&config.startWithVideoMuted=false&config.enableWelcomePage=false&userInfo.displayName=„Oma"'
+fi 
 
 # Set whether to run Chromium in kiosk mode or not
 if [ ! -z ${KIOSK+x} ] && [ "$KIOSK" -eq "1" ]
   then
     echo "Enabling kiosk mode"
-    export CHROME_LAUNCH_URL="--app=$1" 
+    export CHROME_LAUNCH_URL=--app="'$_LOCAL_URL'" 
   else
-    export CHROME_LAUNCH_URL="$1"
+    export CHROME_LAUNCH_URL="'$_LOCAL_URL'"
 fi
 
 # if the PERSISTENT enVar is set, add the appropriate flag
@@ -29,6 +40,7 @@ fi
 
 echo "chromium-browser $CHROME_LAUNCH_URL $KIOSK $FLAGS --window-size=$WINDOW_SIZE" >> /home/chromium/xstart.sh
 
+set +x 
 
 # run script as chromium user
 su -c "export DISPLAY=:$DISPLAY && startx /home/chromium/xstart.sh $CURSOR $OUTPUT" - chromium
